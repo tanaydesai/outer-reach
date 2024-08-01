8d081fb4eb858d9cf426c535ef1983e7aec4949f
TanayDesai <tanaydesai5.com>
2024-08-02 00:56:53 +0530

    I'm back bitch

--- app/page.js
+++ app/page.js
@@ -1,24 +1,24 @@
 "use client"
+import { ChevronRight } from 'lucide-react';
+import { motion } from 'framer-motion';
+
 export default function Home() {
   return (
     <div>
-      <div className='bg-starlink1 bg-cover overflow-x-hidden bg-center w-[100vw] h-full p-8'>
-        <div className='border-white/70 h-screen relative'>        
-          <div className='absolute bottom-10 font-rajdhani-bold text-white w-full'>
-            <h1 className='text-[50px] md:text-[80px] leading-[55px] md:leading-[85px] w-[300px] md:w-[500px]'>THE OUTER REACH</h1>
-            <p className='text-[16px] md:text-[20px] font-rajdhani-regular w-full md:w-[50%]'>This open-source project aims to keep track of the progress and impact of the world's best frontier technology companies that are solving the worlds biggest problems across space, energy, pollution, transport and biology.</p>
-            <div className='flex gap-5 my-3  font-rajdhani-regular'>
-              <button className='p-2 w-[100px] border-2 border-white/70 hover:opacity-70 rounded-sm'>About</button>
-              <button className='p-2 w-[100px] border-2 border-white/70 hover:opacity-70 rounded-sm'>Start</button>
-            </div>
+      <div className='relative bg-cover overflow-x-hidden bg-center w-[100vw] h-screen'>
+          <hr className="absolute left-7 h-full w-[1px] bg-white/[.80]"/>
+          <hr className="absolute right-7 h-full w-[1px] bg-white/[.80]"/>
+
+          <video className='w-full h-full object-cover' autoPlay loop muted>
+            <source src='/videos/wind2.mp4' type='video/mp4'  className="w-full h-auto"/>
+          </video>
+
+          <div className="p-10 absolute bottom-5 md:bottom-9">
+            <h1 className='text-[50px] md:text-[80px] leading-[60px] md:leading-[85px] md:w-[600px] text-white/[.80] font-planer-m'>THE OUTREACH PROJECT</h1>
+            <h3 className='text-[14px] pl-1 md:text-[20px] font-rajdhani-regular text-white/[.70] w-auto md:w-[50%]'>This open-source project aims to keep track of the progress and impact of the world's best frontier technology companies that are solving the worlds biggest problems across space, energy, pollution, transport and biology.</h3>
+            <motion.div whileTap={{scale: 0.97}} className="group text-[11px] mt-5 ml-1 w-fit bg-white px-5 py-3 cursor-default opacity-80 hover:opacity-60 border-[1px] border-white rounded-[11px] font-planer-m tracking-[1.75px] flex items-center gap-1">START NOW <ChevronRight className='mb-0.5 group-hover:translate-x-0.5 transition' size={13}/></motion.div>
           </div>
-        </div>   
       </div>
-      <div className='bg-black bg-cover overflow-x-hidden bg-center w-[100vw] h-full p-8'>
-        <div className='border-white/70 h-screen relative'>
-          <h1 className='text-[60px] md:text-[80px] leading-[85px] text-white w-[500px] absolute bottom-10 font-rajdhani-bold'>THE OUTREACH PROJECT</h1>
-        </div>   
-      </div>   
     </div>
   )
 }
--- components/Navbar/navbar.js
+++ components/Navbar/navbar.js
@@ -1,39 +1,9 @@
 import React from 'react';
-import { FaAngleRight } from 'react-icons/fa';
 
 export const Navbar = () => {
   return (
-    <div className='w-full fixed border-b-2 border-white/70 top-0 h-[60px] flex overflow-auto text-white text-[15px] md:text-[17px] font-rajdhani-bold'>
-        <div className=' group border-r-2 border-white/70 cursor-default flex-1 py-4 px-3'>
-            <div className='w-fit'>
-                SPACE
-                <div class="bg-white h-[2px] w-0 group-hover:w-full transition-all duration-300"></div>
-            </div>   
-        </div>
-        <div className=' border-r-2 group border-white/70 cursor-default flex-1 py-4 px-3 '>
-            <div className='w-fit'>
-                ENERGY
-                <div class="bg-white h-[2px] w-0 group-hover:w-full transition-all duration-300"></div>
-            </div>   
-        </div>
-        <div className=' border-r-2 group border-white/70 cursor-default flex-1 py-4 px-3 '>
-            <div className='w-fit'>
-                POLLUTION
-                <div class="bg-white h-[2px] w-0 group-hover:w-full transition-all duration-300"></div>
-            </div>   
-        </div>
-        <div className=' border-r-2 group border-white/70 cursor-default flex-1 py-4 px-3 '>
-            <div className='w-fit'>
-                TRANSPORT
-                <div class="bg-white h-[2px] w-0 group-hover:w-full transition-all duration-300"></div>
-            </div>   
-        </div>
-        <div className='group border-white/70 cursor-default flex-1 py-4 px-3 '>
-            <div className='w-fit'>
-                BIO
-                <div class="bg-white h-[2px] w-0 group-hover:w-full transition-all duration-300"></div>
-            </div>   
-        </div>
+    <div className='h-[55px] absolute top-0 z-10 border-b-[1px] border-white/[.80] w-full'>
+        
     </div>
   );
 };
