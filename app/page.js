bcfc7a187ee74f5f8a4e670c355fc0b048fa81ce
TanayDesai <tanaydesai5@gmail.com>
2024-10-07 03:57:53 +0530

    New navbar

--- app/page.js
+++ app/page.js
@@ -4,27 +4,47 @@ import Link from 'next/link';
 import { TypeAnimation } from 'react-type-animation';
 import { Fade } from 'react-awesome-reveal';
 import { MoveRight } from 'lucide-react';
+import Image from 'next/image';
+import starship from "@/assests/images/starship.jpg"
+import falcon from "@/assests/images/falcon.jpg"
 
 export default function Home() {
   return (
       <div className='fixed w-full h-full bg-[#efefef] overflow-x-hidden'>
+        <div className="w-full absolute top-0 left-0 -z-10 h-full grid grid-cols-4">
+          {Array.from({ length: 16 }).map((_, index) => (
+            <div key={index} className="relative">
+              <div className="absolute inset-0 flex items-center justify-center">
+                <svg width="7" height="7" viewBox="0 0 10 10" className="text-[#1b1919]">
+                  <path d="M5 0v10M0 5h10" stroke="#1b1919" strokeWidth="0.5" />
+                </svg>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        {/* <Image src={starship} alt="starship" className='w-[150px] h-auto rounded-full absolute top-[150px] left-[150px]' />
+        <Image src={falcon} alt="starship" className='w-[150px] h-auto rounded-full  absolute bottom-[80px] left-[150px]' />
+        <Image src={starship} alt="starship" className='w-[150px] h-auto rounded-full absolute top-[80px] right-[150px]' />
+        <Image src={starship} alt="starship" className='w-[150px] h-auto rounded-full absolute bottom-[150px] right-[150px]' /> */}
+
         <div className='absolute w-full h-full flex items-center justify-center'>
             <div> 
               <div className='flex justify-center'><Fade><TypeAnimation speed={40} cursor={false} sequence={["{Outer", "{Outer,", '{Outer, Reach}']} className='text-[45px] md:text-[80px] text-center leading-[60px] md:leading-[85px] text-[#1e1f2b] uppercase font-a2'/></Fade></div>
-              <div className='mt-5 mx-auto w-[400px] text-center text-[16px] text-[#1e1f2b] font-a1'><Fade>Data to Track Progress of the world's frontier technologies</Fade></div>
+              <div className='mt-5 mx-auto w-[400px] text-center text-[16px] text-[#1e1f2b] font-a1'><Fade>Data to demistify widely held misperceptions and track the progress of humanity's frontier technologies.</Fade></div>
               {/* <Fade><Link href="/energy"><div className='mt-5 group mx-auto w-[400px] text-center text-[16px] text-[#1e1f2b] font-a1 flex items-center justify-center gap-2'>Start<div className='group-hover:translate-x-1 pt-1 duration-300'><MoveRight size={14}/></div></div></Link></Fade> */}
             </div>
         </div>
         <Fade>
-        <div className='absolute bottom-0 px-5 p-4 text-[12.5px] w-full flex items-end text-[#1e1f2b] justify-between uppercase font-rajdhani-regular'>
-          <div className='border-l-[2px] h-[55px] md:h-[36px] break-words w-[160px] md:w-[200px] border-l-[#9b9b9b] px-2'>
+        <div className='absolute bottom-0 px-5 p-4 text-[12.5px] w-full flex items-end text-[#9b9b9b] justify-between uppercase font-rajdhani-regular'>
+          <div className='border-l-[2px] h-[55px] md:h-[36px] break-words w-[160px] md:w-[200px] border-l-[#c3c3c3] px-2'>
             <div><TypeAnimation speed={40} cursor={false} sequence={["Optimistic", "Optimistic, accelerationist", "Optimistic, accelerationist & technocapitalistic"]} /></div>
           </div>
-          <div className='hidden md:block border-l-[2px] h-[36px] border-l-[#9b9b9b] px-2'>
+          <div className='hidden md:block border-l-[2px] h-[36px] border-l-[#c3c3c3] px-2'>
             <div><TypeAnimation speed={40} cursor={false} sequence={["Open","Open Source"]}/></div>
             <Link href="https://github.com/tanaydesai" target="_blank"><div className='group cursor-default'><TypeAnimation speed={40} cursor={false} sequence={["GitHub"]} /><div className='ml-1 group-hover:translate-x-1 duration-300 inline-flex'>/</div></div></Link>
           </div>
-          <div className='border-l-[2px] h-[55px] md:h-[36px] border-l-[#9b9b9b] px-2'>
+          <div className='border-l-[2px] h-[55px] md:h-[36px] border-l-[#c3c3c3] px-2'>
             <div><TypeAnimation speed={40} cursor={false} sequence={["Est.", "Est. 2024"]} /></div>
             <Link href="https://tanaydesai.com" target="_blank"><div className='group cursor-default'><TypeAnimation speed={40} cursor={false} sequence={["Tanay","Tanay Desai",]} /><div className='ml-1 group-hover:translate-x-1 duration-300 inline-flex'>/</div></div></Link>
             <Link href="https://github.com/tanaydesai" target="_blank"><div className='block md:hidden group cursor-default'><TypeAnimation speed={40} cursor={false} sequence={["GitHub"]} /><div className='ml-1 group-hover:translate-x-1 duration-300 inline-flex'>/</div></div></Link>
--- components/navbar.js
+++ components/navbar.js
@@ -4,21 +4,43 @@ import { SunMediumIcon, AtomIcon, RocketIcon, LeafIcon, Zap, ZapIcon, MoveRight,
 import { motion } from 'framer-motion';
 import { Fade } from 'react-awesome-reveal';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export const Navbar = () => {
   const [open, setOpen] = useState(false);
+  const path = usePathname();
  
   return (
-    <div className={`fixed top-0 z-10 w-full left-0 px-6 py-2 flex items-center justify-between ${open ? "" : "backdrop-blur-[10px]"}`}>
+    <div className={`fixed top-0 z-10 w-full left-0 px-5 py-3 flex bg-transparent items-start justify-between ${open ? "" : "backdrop-blur-[10px]"}`}>
       <Link href="/"><motion.div whileTap={{scale:1.1}} className='w-fit cursor-default text-[#9b9b9b] text-[18px] uppercase font-rajdhani-regular flex items-center gap-1'>{'0'} <MoveRight size={15}/> {'1'}</motion.div></Link>
-      <div className='hidden md:flex justify-center text-[17px] cursor-default items-center text-[#9b9b9b] font-a1 w-fit gap-3'>
+     
+      {/* <div className='hidden md:flex justify-center text-[17px] cursor-default items-center text-[#9b9b9b] font-a1 w-fit gap-3'>
         <Link href="/energy"><div className='group hover:text-[#1e1f2b] duration-300'><div class="bg-[#24252f] h-[1px] my-1 w-0 group-hover:w-[90%] transition-all duration-700"></div>Energy<div className='ml-2 group-hover:translate-x-1 duration-300 inline-flex'>/</div></div></Link>
         <Link href="/nuclear"><div className='group hover:text-[#1e1f2b] duration-300'><div class="bg-[#24252f] h-[1px] my-1 w-0 group-hover:w-[90%] transition-all duration-700"></div>Nuclear<div className='ml-2 group-hover:translate-x-1 duration-300 inline-flex'>/</div></div></Link>
         <Link href="/ai"><div className='group hover:text-[#1e1f2b] duration-300'><div class="bg-[#24252f] h-[1px] my-1 w-0 group-hover:w-[90%] transition-all duration-700"></div>Artificial Intelligence<div className='ml-2 group-hover:translate-x-1 duration-300 inline-flex'>/</div></div></Link>
-        {/* <Link href="/ev"><div className='group hover:text-[#1e1f2b] duration-300'><div class="bg-[#24252f] h-[1px] my-1 w-0 group-hover:w-[90%] transition-all duration-700"></div>Electric Vehicles<div className='ml-2 group-hover:translate-x-1 duration-300 inline-flex'>/</div></div></Link> */}
         <Link href="/space"><div className='group hover:text-[#1e1f2b] duration-300'><div class="bg-[#24252f] h-[1px] my-1 w-0 group-hover:w-[90%] transition-all duration-700"></div>Space<div className='ml-2 group-hover:translate-x-1 duration-300 inline-flex'>/</div></div></Link>
-      </div>
-      <motion.div whileTap={{scale:1.1}} className='block md:hidden' onClick={() => setOpen(true)}><LayersIcon size={18} className='text-[#9b9b9b] hover:text-[#000000] duration-300'/></motion.div>
+      </div> */}
+
+      {path === '/' && 
+        <div className='grid grid-rows-4 place-items-end text-[15px] cursor-default text-[#9b9b9b] font-a1 w-fit gap-3'>
+          <Link href="/energy"><motion.div whileHover={{x:-10, transition:{duration:0.6, type:'spring'}}} className='group hover:text-[#1e1f2b]'>Road to Energy Abundance<div className='ml-2 duration-300 inline-flex'>/</div></motion.div></Link>
+          <Link href="/nuclear"><motion.div whileHover={{x:-10, transition:{duration:0.6, type:'spring'}}} className='group hover:text-[#1e1f2b]'>Nuclear's Revenge<div className='ml-2 duration-300 inline-flex'>/</div></motion.div></Link>
+          <Link href="/ai"><motion.div whileHover={{x:-10, transition:{duration:0.6, type:'spring'}}} className='group hover:text-[#1e1f2b]'>Age of AGI<div className='ml-2 duration-300 inline-flex'>/</div></motion.div></Link>
+          <Link href="/space"><motion.div whileHover={{x:-10, transition:{duration:0.6, type:'spring'}}} className='group hover:text-[#1e1f2b]'>The Space Race<div className='ml-2 duration-300 inline-flex'>/</div></motion.div></Link>
+        </div> 
+      }
+      
+      {/* {path === '/' && 
+        <div className='grid grid-rows-4 place-items-end text-[15px] cursor-default text-[#9b9b9b] font-a1 w-fit gap-3'>
+          <Link href="/energy"><motion.div whileHover={{x:-10, transition:{duration:0.5, type:'spring'}}} className='group w-fit hover:text-[#1e1f2b] rounded-[7px] hover:border-[#1e1f2b] border-[#9b9b9b] border-2 px-2'>Road to Energy Abundance</motion.div></Link>
+          <Link href="/energy"><motion.div whileHover={{x:-10, transition:{duration:0.5, type:'spring'}}} className='group w-fit hover:text-[#1e1f2b] rounded-[7px] hover:border-[#1e1f2b] border-[#9b9b9b] border-2 px-2'>Nuclear's Revenge</motion.div></Link>
+          <Link href="/energy"><motion.div whileHover={{x:-10, transition:{duration:0.5, type:'spring'}}} className='group w-fit hover:text-[#1e1f2b] rounded-[7px] hover:border-[#1e1f2b] border-[#9b9b9b] border-2 px-2'>Age of AGI</motion.div></Link>
+          <Link href="/energy"><motion.div whileHover={{x:-10, transition:{duration:0.5, type:'spring'}}} className='group w-fit hover:text-[#1e1f2b] rounded-[7px] hover:border-[#1e1f2b] border-[#9b9b9b] border-2 px-2'>Space Race</motion.div></Link>
+        </div> 
+      } */}
+     
+     {path !== '/' && <motion.div whileTap={{scale:1.1}} className='block md:hidden' onClick={() => setOpen(true)}><LayersIcon size={18} className='text-[#9b9b9b] hover:text-[#000000] duration-300'/></motion.div>}
+     
       {open && 
       <div className='fixed z-[100] px-6 p-4 text-[40px] font-rajdhani-regular w-full h-full bg-[#d0d0d0]/30 backdrop-blur-[15px] top-0 left-0'>
         <XIcon size={20} className='mt-1 mr-0 ml-auto' onClick={() => setOpen(false)}/>
@@ -28,7 +50,6 @@ export const Navbar = () => {
           <Link href="/energy"><div onClick={() => setOpen(false)} className='mt-5 group w-full rounded-md px-1 hover:bg-[#e0dfdf]'>Energy<ChevronRight className='ml-2 group-hover:translate-x-1 duration-300 inline-flex'/></div></Link>
           <Link href="/nuclear"><div onClick={() => setOpen(false)} className='group w-full rounded-md px-1 hover:bg-[#e0dfdf]'>Nuclear<ChevronRight className='ml-2 group-hover:translate-x-1 duration-300 inline-flex'/></div></Link>
           <Link href="/ai"><div onClick={() => setOpen(false)} className='group w-full rounded-md px-1 hover:bg-[#e0dfdf]'>AI<ChevronRight className='ml-2 group-hover:translate-x-1 duration-300 inline-flex'/></div></Link>
-          {/* <Link href="/ev"><div onClick={() => setOpen(false)} className='group w-full rounded-md px-1 hover:bg-[#e0dfdf]'>Electric Vehicles<ChevronRight className='ml-2 group-hover:translate-x-1 flex-nowrap shrink-0 duration-300 inline-flex'/></div></Link> */}
           <Link href="/space"><div onClick={() => setOpen(false)} className='group w-full rounded-md px-1 hover:bg-[#e0dfdf]'>Space<ChevronRight className='ml-2 group-hover:translate-x-1 duration-300 inline-flex'/></div></Link>
         </Fade>
       </div>}
