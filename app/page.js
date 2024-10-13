4df96822c8eac081ff9255f43577307d48b405a8
TanayDesai <tanaydesai5@gmail.com>
2024-10-14 05:21:47 +0530

    size fixes

--- app/page.js
+++ app/page.js
@@ -8,15 +8,15 @@ import {DecryptText} from '@/components/decrypt'
 
 export default function Home() {
   return (
-      <div className='absolute w-full min-h-full px-3 py-[50px] bg-[#efefef]'>
+      <div className='absolute w-full min-h-full px-3 py-[70px] bg-[#efefef]'>
   
         <div className='w-full h-fit'> 
-          <Fade className='h-[65px] sm:h-[80px] w-full'><TypeAnimation speed={10} cursor={false} sequence={[1000,"Welcome", "Welcome to", 'Welcome to']} className='text-[45px] md:text-[70px] lg:text-[100px] whitespace-nowrap leading-[50px] md:leading-[85px] text-[#463f3a] font-a2'/></Fade>
-          <Fade className='h-[80px] w-full flex justify-end md:mt-4'><TypeAnimation speed={20} cursor={false} sequence={[2400,"The Outer Reach"]} className='text-[45px] md:text-[70px] lg:text-[100px] leading-[50px] md:leading-[85px] text-[#463f3a] font-a2'/></Fade>
+          <Fade className='h-[70px] sm:h-[80px] w-full'><TypeAnimation speed={10} cursor={false} sequence={[1000,"Welcome", "Welcome to", 'Welcome to']} className='text-[50px] md:text-[90px] lg:text-[120px] whitespace-nowrap leading-[85px] md:leading-[85px] text-[#463f3a] font-a2'/></Fade>
+          <Fade className='min-h-[90px] w-full flex justify-start md:mt-6 lg:mt-10'><TypeAnimation speed={20} cursor={false} sequence={[2400,"The Outer Reach"]} className='text-[50px] md:text-[90px] lg:text-[120px] leading-[70px] md:leading-[85px] text-[#463f3a] font-a2'/></Fade>
         
-          <div className='w-full mt-10'>
+          <div className='w-full mt-10 sm:mt-[100px]'>
               <div className='flex items-center pb-1 h-[27px]'><div className='w-2 h-2 bg-red-400 mr-2 inline-flex'/><DecryptText className='text-[12px] uppercase font-a2 text-[#463f3a]' text='Description' /></div>
-              <div className='text-[17px] sm:text-[20px] w-full md:w-[90%] lg:w-[60%] text-[#463f3a] font-a1'>Data insights to track the progress of humanity's frontier technologies, highlight their need and impact and demistify societal misconceptions.</div>
+              <div className='text-[18px] sm:text-[23px] w-full lg:w-[70%] font-semibold text-[#463f3a] font-a1'>Data insights to track the progress of humanity's frontier technologies, highlight their need and impact and demistify societal misconceptions.</div>
               <div className='text-[#9b9b9b] text-[17px] h-[27px] mt-5 font-a1 gap-2 flex items-center'><div className='text-[13px]'>{`{01}`}</div><DecryptText className='text-[17px] text-[#9b9b9b]' text='Accessible & comphrensive.'/></div>
               <div className='text-[#9b9b9b] font-a1 gap-2 h-[27px] mt-1 flex items-center'><div className='text-[13px]'>{`{02}`}</div><DecryptText className='text-[17px] text-[#9b9b9b]' text='Beginner-friendly & visual.' /></div>
               <div className=' text-[#9b9b9b] font-a1 gap-2 h-[27px] mt-1 flex items-center'><div className='text-[13px]'>{`{03}`}</div><DecryptText className='text-[17px] text-[#9b9b9b]' text='Techno-optimistic.' /></div>
--- components/navbar.js
+++ components/navbar.js
@@ -11,7 +11,7 @@ export const Navbar = () => {
   const path = usePathname();
  
   return (
-    <div className={`fixed top-0 z-10 w-full left-0 px-3 py-2 flex bg-transparent items-center justify-between ${open ? "" : "backdrop-blur-[10px]"}`}>
+    <div className={`fixed top-0 z-10 w-full left-0 px-3 py-3 flex bg-transparent items-center justify-between ${open ? "" : "backdrop-blur-[10px]"}`}>
       <Link href="/"><motion.div whileTap={{scale:1.1}} className='w-fit cursor-default text-[#9b9b9b] text-[12px] uppercase font-a2 rounded-[5px] border border-[#b3b3b3] px-2 flex items-center gap-1'>{'0'} <MoveRight size={15}/> {'42'}</motion.div></Link>
     
       <div className='flex gap-1 text-[12px] uppercase text-[#9b9b9b] font-a2 '>
