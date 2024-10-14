59a05f962bd665873bd1998d6914e3ea1c9fbe2d
TanayDesai <tanaydesai5@gmail.com>
2024-10-14 22:06:55 +0530

    fixes, onto domain structuring

--- app/page.js
+++ app/page.js
@@ -15,15 +15,15 @@ export default function Home() {
   
         <div className='w-full h-fit'> 
           <Fade className='h-[70px] sm:h-[80px] w-full'><TypeAnimation speed={10} cursor={false} sequence={[1000,"Welcome", "Welcome to", 'Welcome to']} className='text-[65px] md:text-[90px] lg:text-[120px] whitespace-nowrap leading-[85px] md:leading-[85px] text-[#463f3a] font-a2'/></Fade>
-          <Fade className='min-h-[90px] w-full flex justify-start md:mt-6 lg:mt-10'><TypeAnimation speed={20} cursor={false} sequence={[2400,"The Outer Reach"]} className='text-[65px] md:text-[90px] lg:text-[120px] leading-[70px] md:leading-[85px] text-[#463f3a] font-a2'/></Fade>
-        
+          <Fade className='min-h-[90px] w-full flex justify-end md:mt-6 lg:mt-10'><TypeAnimation speed={20} cursor={false} sequence={[2400,"Outer Reach"]} className='text-[65px] md:text-[90px] lg:text-[120px] leading-[70px] md:leading-[85px] text-[#463f3a] font-a2'/></Fade>
+         
           <div className='w-full mt-10 sm:mt-[100px]'>
               <div className='flex items-center h-[27px]'><div className='w-2 h-2 bg-red-400 mr-2 inline-flex'/><DecryptText className='text-[13px] uppercase font-a2 text-[#463f3a]' text='Description' /></div>
               <div className='text-[16px] sm:text-[22px] mt-2 lg:text-[24px] w-full md:w-[85%] lg:w-[70%] leading-6 sm:leading-8 font-semibold text-[#463f3a] font-a1'>Data insights to track the progress of humanity's frontier technologies, highlight their need and impact and demistify societal misconceptions.</div>
               <div className='md:flex gap-4 mt-5'>
-                <div className='text-[#9b9b9b] h-[27px] font-a1 gap-2 flex items-center'><div className='text-[12px]'>{`{01}`}</div><DecryptText className='text-[14px] sm:text-[17px] text-[#9b9b9b]' text='Accessible & comphrensive'/></div>
-                <div className='text-[#9b9b9b] md:mt-0  font-a1 gap-2 h-[27px] flex items-center'><div className='text-[12px]'>{`{02}`}</div><DecryptText className='text-[14px] sm:text-[17px] text-[#9b9b9b]' text='Beginner-friendly & visual' /></div>
-                <div className=' text-[#9b9b9b] md:mt-0  font-a1 gap-2 h-[27px] flex items-center'><div className='text-[12px]'>{`{03}`}</div><DecryptText className='text-[14px] sm:text-[17px] text-[#9b9b9b]' text='Techno-optimistic' /></div>
+                <div className='text-[#9b9b9b] h-[27px] font-a1 gap-2 flex items-center'><div className='text-[12px]'>{`{01}`}</div><DecryptText className='text-[15px] sm:text-[17px] text-[#9b9b9b]' text='Accessible & comphrensive'/></div>
+                <div className='text-[#9b9b9b] md:mt-0  font-a1 gap-2 h-[27px] flex items-center'><div className='text-[12px]'>{`{02}`}</div><DecryptText className='text-[145x] sm:text-[17px] text-[#9b9b9b]' text='Beginner-friendly & visual' /></div>
+                <div className=' text-[#9b9b9b] md:mt-0  font-a1 gap-2 h-[27px] flex items-center'><div className='text-[12px]'>{`{03}`}</div><DecryptText className='text-[15px] sm:text-[17px] text-[#9b9b9b]' text='Techno-optimistic' /></div>
               </div>
           </div>
         
@@ -43,8 +43,8 @@ export default function Home() {
 
         </div>
 
-        <div className="w-full absolute pointer-events-none -z-1 top-0 left-0 h-full grid grid-cols-4">
-          {Array.from({ length: 20 }).map((_, index) => (
+        <div className="w-full absolute pointer-events-none -z-1 top-0 left-0 h-screen grid grid-cols-4">
+          {Array.from({ length: 12 }).map((_, index) => (
             <div key={index} className="relative">
               <div className="absolute inset-0 flex items-center justify-center">
                 <svg width="10" height="10" viewBox="0 0 10 10" className="text-[#463f3a]">
